25923d37a984b49c00f0114c8da6314820788a94
James Guppy <j.guppy321@gmail.com>
2023-05-13 20:13:19 +0100

    Added GraphQL schemas to filter bands based on ID number. Also schema to filter albums based on band ID. These may change later depending on frontend functionality but should work for initial MVP project.

--- bands/schema.py
+++ bands/schema.py
@@ -34,12 +34,18 @@ class AlbumType(DjangoObjectType):
 class Query(graphene.ObjectType):
 
     all_bands = graphene.List(BandType)
+    filter_bands = graphene.Field(BandType, id=graphene.Int())
     all_albums = graphene.List(AlbumType)
+    filter_albums = graphene.List(AlbumType, id=graphene.Int())
 
     def resolve_all_bands(self, info):
         return Band.objects.all()
+    def resolve_filter_bands(self, info, id):
+        return Band.objects.get(pk=id)
     def resolve_all_albums(self, info):
         return Album.objects.all()
+    def resolve_filter_albums(self, info, id):
+        return Album.objects.filter(artist=id)
 
 
 
